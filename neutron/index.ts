938e22019001ac32ed667eace06faa7395578805
hugo <hugo.perier@cern.ch>
2022-10-09 23:11:55 +0200

    fix path

--- neutron/index.ts
+++ neutron/index.ts
@@ -20,13 +20,10 @@ export { TopicSettings } from "./interfaces/ros";
 // Modules
 export { Core, ICoreProcess, ICoreModule } from "./modules/Core";
 export { ICameraConfiguration, Camera } from "./modules/Camera";
-export {
-  IRobotBaseConfiguration,
-  RobotBase,
-} from "./modules/Robotbase";
+export { IRobotBaseConfiguration, RobotBase } from "./modules/RobotBase";
 
 // Utils
 export { inRange } from "./utils/math";
 export { LiteEventHandler, ILiteEvent, LiteEvent } from "./utils/LiteEvent";
 export { isEmpty, isBlank, capitalize } from "./utils/string";
-export {ILoggerMessage, LogType, Logger} from "./utils/Logger";
+export { ILoggerMessage, LogType, Logger } from "./utils/Logger";
